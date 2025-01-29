604cf2fc7d74bb7b737d87327993558363b88e7f
Reza Dezhhoot <rdezhhoot@gmail.com>
2025-01-29 13:02:11 +0330

    a

--- config/filesystems.php
+++ config/filesystems.php
@@ -75,6 +75,7 @@ return [
 
     'links' => [
         public_path('storage') => storage_path('app/public'),
+//        realpath(base_path().'/../public_html') => storage_path('app/public'),
     ],
 
 ];
