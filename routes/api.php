3e345c3f9156c95962aefea34b0df921337ea58f
Reza Dezhhoot <rdezhhoot@gmail.com>
2025-01-29 04:32:40 +0330

    add profile

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'v1'] , function () {
+    Route::get('users/profile' , \App\Http\Controllers\Api\V1\UserController::class)->middleware('auth:sanctum');
     Route::get('dashboard-items' , \App\Http\Controllers\Api\V1\DashboardItemController::class);
     Route::get('banners' , \App\Http\Controllers\Api\V1\BannerController::class);
     Route::apiResource('request-plans' , \App\Http\Controllers\Api\V1\RequestPlanController::class)->middleware('auth:sanctum')->only(['index','show']);
