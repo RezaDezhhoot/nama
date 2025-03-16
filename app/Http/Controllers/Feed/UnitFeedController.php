712639ce09f4f297f130d2b5a32107950e7e2ef3
Reza Dezhhoot <rdezhhoot@gmail.com>
2025-03-16 13:53:29 +0330

    update

---
+++ app/Http/Controllers/Feed/UnitFeedController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Feed;
+
+use App\Http\Controllers\Controller;
+use App\Models\Unit;
+use Illuminate\Http\Request;
+
+class UnitFeedController extends Controller
+{
+    public function __invoke(Request $request , bool $parent = true)
+    {
+        $items = Unit::query()->when($parent , function ($q) {
+            $q->whereNull('parent_id');
+        })->search($request->get('search'))->take(15)->select2()->get();
+        return response()->json($items);
+    }
+}
