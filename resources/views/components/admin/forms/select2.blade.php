0d9e6d468fca381e42265111ef8b9c0b42142e5d
Reza Dezhhoot <rdezhhoot@gmail.com>
2025-04-19 14:48:11 +0330

    fix bug

--- resources/views/components/admin/forms/select2.blade.php
+++ resources/views/components/admin/forms/select2.blade.php
@@ -3,8 +3,83 @@
     <div  >
         <label for="{{$id}}"> {{$label}} <span {{ ! $required ? 'hidden' : '' }} class="text-danger">*</span></label>
         <div class=" d-flex align-items-center" wire:ignore>
-            <select  id="{{$id}}" multiple
-                     class="form-control select2 "  >
+            <select  id="{{$id}}" multiple class="form-control select2"  x-init="
+                    Livewire.on('attachSelect2#{{$id}}' , function ([data], text = 'title') {
+                        console.log(data)
+                        var newOption = new Option(data[text], data.id, true, true);
+                        $('#{{$id}}').append(newOption).trigger('change')
+                    })
+                    Livewire.on('clear#{{$id}}' , function (ids, text = 'title') {
+                       $('#{{$id}}').empty();
+                    })
+                    Livewire.on('reloadSelect2#{{$id}}' , function (data , text = 'title' , multiple = true) {
+                        $('#{{$id}}').empty();
+                        if (multiple) {
+                            data.forEach(item => {
+                                var newOption = new Option(item[text], item.id, true, true);
+                                $('#{{$id}}').append(newOption).trigger('change')
+                            })
+                        } else {
+                            var newOption = new Option(data[text], data.id, true, true);
+                            $('#{{$id}}').append(newOption).trigger('change')
+                        }
+                    })
+                    $(document).ready(() => {
+                        $('#{{$id}}').select2({
+                            placeholder: 'select',
+                            allowClear: true,
+                            multiple: '{{$multiple}}',
+                            ajax: {
+                                url:  '{{$ajaxUrl}}',
+                                data: function (params) {
+                                    var query = {
+                                        search: params.term,
+                                        type: 'public'
+                                    }
+                                    return query;
+                                },
+                                processResults: function (data) {
+                                    return {
+                                        results: data
+                                    };
+                                }
+                            }
+                        })
+                        $('#{{$id}}').on('change', function (e) {
+                            var data = $('#{{$id}}').select2('val');
+                            @this.set('{{$attributes->wire("model")->value}}', data);
+                        });
+                        $('#{{$id}}').val($('#{{$id}}').select2('val'));
+                        $('#{{$id}}').trigger('change');
+                        $('#clear{{$id}}').on('click', function (e) {
+                            $('#{{$id}}').empty();
+                            @this.set('{{$attributes->wire("model")->value}}', null);
+                        });
+
+                        Livewire.on('reloadAjaxURL#{{$id}}' , function ([url]) {
+                            $('#{{$id}}').select2({
+                                placeholder: 'select',
+                                allowClear: true,
+                                multiple: '{{$multiple}}',
+                                ajax:{
+                                    url,
+                                    data: function (params) {
+                                        var query = {
+                                            search: params.term,
+                                            type: 'public'
+                                        }
+                                        return query;
+                                    },
+                                    processResults: function (data) {
+                                        return {
+                                            results: data
+                                        };
+                                    }
+                                }
+                            })
+                        })
+                    })
+            ">
                 <option value="">-</option>
                 @if($data && is_array($data) && sizeof($data) > 0)
                     @if($multiple)
@@ -19,84 +94,26 @@
             </select>
         </div>
     </div>
+    @error($attributes->wire("model")->value)
+    <small class="text-danger d-block">{{ $message }}</small>
+    @enderror
 </div>
 @push('scripts')
     <script>
         document.addEventListener('livewire:init', () => {
-            Livewire.on('attachSelect2#{{$id}}' , function (data, text = 'title') {
-                var newOption = new Option(data[text], data.id, true, true);
-                $('#{{$id}}').append(newOption).trigger('change')
-            })
-            Livewire.on('reloadSelect2#{{$id}}' , function (data , text = 'title' , multiple = true) {
-                $('#{{$id}}').empty();
-                if (multiple) {
-                    data.forEach(item => {
-                        var newOption = new Option(item[text], item.id, true, true);
-                        $('#{{$id}}').append(newOption).trigger('change')
+            Livewire.on('updateSelect2' , function ([data]) {
+                $(`#${data['id']}`).empty();
+                if (data['multiple']) {
+                    data['data'].forEach(item => {
+                        var newOption = new Option(item[data['text'] ?? 'text'], item['id'], true, true);
+                        $(`#${data['id']}`).append(newOption).trigger('change')
                     })
                 } else {
-                    var newOption = new Option(data[text], data.id, true, true);
-                    $('#{{$id}}').append(newOption).trigger('change')
+                    var newOption = new Option(data['data'][data['text'] ?? 'text'], data['data']['id'], true, true);
+                    $(`#${data['id']}`).append(newOption).trigger('change')
                 }
             })
-            $(document).ready(() => {
-                $('#{{$id}}').select2({
-                    placeholder: "select",
-                    allowClear: true,
-                    multiple: '{{$multiple}}',
-                    ajax:{
-                        url:  '{{$ajaxUrl}}',
-                        data: function (params) {
-                            var query = {
-                                search: params.term,
-                                type: 'public'
-                            }
-                            return query;
-                        },
-                        processResults: function (data) {
-                            return {
-                                results: data
-                            };
-                        }
-                    }
-                })
-                $('#{{$id}}').on('change', function (e) {
-                    var data = $('#{{$id}}').select2("val");
-                @this.set('{{$attributes->wire("model")->value}}', data);
-                });
-                $('#{{$id}}').val($('#{{$id}}').select2("val"));
-                $('#{{$id}}').trigger('change');
-                $('#clear{{$id}}').on('click', function (e) {
-                    $('#{{$id}}').empty();
-                @this.set('{{$attributes->wire("model")->value}}', null);
-                });
-
-                Livewire.on('reloadAjaxURL#{{$id}}' , function (url) {
-                    $('#{{$id}}').select2({
-                        placeholder: "select",
-                        allowClear: true,
-                        multiple: '{{$multiple}}',
-                        ajax:{
-                            url,
-                            data: function (params) {
-                                var query = {
-                                    search: params.term,
-                                    type: 'public'
-                                }
-                                return query;
-                            },
-                            processResults: function (data) {
-                                console.log(data)
-                                return {
-                                    results: data
-                                };
-                            }
-                        }
-                    })
-
-                })
-            })
-        });
+        })
 
     </script>
 @endpush
