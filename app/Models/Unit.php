18d32188743279d402e96027481e3195a0d37156
Reza Dezhhoot <rdezhhoot@gmail.com>
2025-05-14 16:21:48 +0330

    fix bug

--- app/Models/Unit.php
+++ app/Models/Unit.php
@@ -47,7 +47,7 @@ class Unit extends Model
     public function full(): Attribute
     {
         return Attribute::get(function ($v) {
-            return sprintf("%s- %s - %s",$this->title,$this->parent_id ? "معمولی" : 'محوری' ,  $this->sub_type );
+            return sprintf("%s- %s - %s",$this->title,$this->parent_id ? "معمولی" : 'محوری' ,  $this->sub_type?->value );
         });
     }
 
