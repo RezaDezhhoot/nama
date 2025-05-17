ed2242b91e6809d6da906f62af972cdc8a70e0df
Reza Dezhhoot <rdezhhoot@gmail.com>
2025-05-17 13:16:11 +0330

    fix gut

--- app/Http/Requests/Api/V1/SubmitRequest.php
+++ app/Http/Requests/Api/V1/SubmitRequest.php
@@ -20,8 +20,8 @@ class SubmitRequest extends FormRequest
             'amount' => ['required','integer','between:1000,10000000000000'],
             'date' => ['required'],
             'body' => ['nullable','max:10000'],
-            'imam_letter' => ['nullable',Rule::file()->extensions([...config('site.files.image.formats'),...config('site.files.document.formats')])->max(5 * 1024)],
-            'area_interface_letter' => ['nullable',Rule::file()->extensions([...config('site.files.image.formats'),...config('site.files.document.formats')])->max(5 * 1024)],
+            'imam_letter' => ['nullable',Rule::file()->max(50 * 1024)],
+            'area_interface_letter' => ['nullable',Rule::file()->max(50 * 1024)],
             'request_plan_id' => ['required','integer','min:1',Rule::exists('request_plans','id')->where('status',RequestPlanStatus::PUBLISHED)],
             'sheba' => ['nullable',new ShebaNumberRule]
         ];
