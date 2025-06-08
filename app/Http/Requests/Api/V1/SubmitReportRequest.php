9a2c57f41a9d351e57da7f56dcff1e613126b9c6
Reza Dezhhoot <rdezhhoot@gmail.com>
2025-06-08 23:45:46 +0330

    update

--- app/Http/Requests/Api/V1/SubmitReportRequest.php
+++ app/Http/Requests/Api/V1/SubmitReportRequest.php
@@ -15,8 +15,8 @@ class SubmitReportRequest extends FormRequest
             'date' => ['required','date'],
             'body' => ['nullable','max:10000'],
             'images' => ['required','array','max:10'],
-            'images.*' => ['required',Rule::file()->extensions(config('site.files.image.formats'))->max(100 * 1024)],
-            'video' => ['nullable',Rule::file()->extensions(config('site.files.video.formats'))->max(100 * 1024)],
+            'images.*' => ['required',Rule::file()->max(200 * 1024)],
+            'video' => ['nullable',Rule::file()->max(200 * 1024)],
         ];
     }
 }
