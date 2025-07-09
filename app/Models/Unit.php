390ea54ef9acf23c1d5a584f2a903dabd84458f2
Reza Dezhhoot <rdezhhoot@gmail.com>
2025-07-09 12:51:16 +0330

    fix bug

--- app/Models/Unit.php
+++ app/Models/Unit.php
@@ -9,10 +9,11 @@ use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Unit extends Model
 {
-    use SimpleSearchable;
+    use SimpleSearchable , SoftDeletes;
 
     public array $searchAbleColumns = ['title'];
 
