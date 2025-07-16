8537aa3a75d0c761e68c1acc8aa2f74692f9a31e
Reza Dezhhoot <rdezhhoot@gmail.com>
2025-07-16 12:41:10 +0330

    add soft delete to user roles

--- app/Models/UserRole.php
+++ app/Models/UserRole.php
@@ -6,9 +6,12 @@ use App\Enums\OperatorRole;
 use App\Enums\SchoolCoachType;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class UserRole extends Model
 {
+    use SoftDeletes;
+
     protected $guarded = ['id'];
     protected $table = 'user_roles';
     protected $connection = 'mysql';
