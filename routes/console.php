f0b02fc90d968d2f3ca819c9bbeda28bc2656db0
Reza Dezhhoot <rdezhhoot@gmail.com>
2025-08-06 02:16:57 +0330

    update

--- routes/console.php
+++ routes/console.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Artisan;
 
 
 \Illuminate\Support\Facades\Schedule::command("app:prepare-daily-accounting-record-command mosque --subType=brothers")->dailyAt("12:00");
-\Illuminate\Support\Facades\Schedule::command("app:prepare-daily-accounting-record-command mosque --subType=sisters")->dailyAt("12:05");
-\Illuminate\Support\Facades\Schedule::command("app:prepare-daily-accounting-record-command school --subType=male")->dailyAt("12:10");
-\Illuminate\Support\Facades\Schedule::command("app:prepare-daily-accounting-record-command school --subType=female")->dailyAt("12:15");
-\Illuminate\Support\Facades\Schedule::command("app:prepare-daily-accounting-record-command school --subType=support")->dailyAt("12:20");
-\Illuminate\Support\Facades\Schedule::command("app:prepare-daily-accounting-record-command center")->dailyAt("12:25");
-\Illuminate\Support\Facades\Schedule::command("app:prepare-daily-accounting-record-command university")->dailyAt("12:30");
+\Illuminate\Support\Facades\Schedule::command("app:prepare-daily-accounting-record-command mosque --subType=sisters")->dailyAt("12:00");
+\Illuminate\Support\Facades\Schedule::command("app:prepare-daily-accounting-record-command school --subType=male")->dailyAt("12:00");
+\Illuminate\Support\Facades\Schedule::command("app:prepare-daily-accounting-record-command school --subType=female")->dailyAt("12:00");
+\Illuminate\Support\Facades\Schedule::command("app:prepare-daily-accounting-record-command school --subType=support")->dailyAt("12:00");
+\Illuminate\Support\Facades\Schedule::command("app:prepare-daily-accounting-record-command center")->dailyAt("12:00");
+\Illuminate\Support\Facades\Schedule::command("app:prepare-daily-accounting-record-command university")->dailyAt("12:00");
