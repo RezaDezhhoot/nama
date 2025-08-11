6bde8c55b39ec6d5084012b19612952661bb13db
Reza Dezhhoot <rdezhhoot@gmail.com>
2025-08-11 22:44:22 +0330

    update

--- app/Http/Controllers/Api/V1/Inquiry/ReportController.php
+++ app/Http/Controllers/Api/V1/Inquiry/ReportController.php
@@ -18,6 +18,7 @@ class ReportController extends Controller
         }
         $items = [];
         Report::query()
+            ->with(['images','images2','video','otherVideos'])
             ->where('reports.step',RequestStep::FINISH)
             ->join('requests AS r','r.id','=','reports.request_id')
             ->select(['reports.*','r.unit_id AS unit_id','r.user_id AS user_id'])
