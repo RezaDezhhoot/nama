bed9690be581eb0f5fcb325cfd6c69d935286f12
Reza Dezhhoot <rdezhhoot@gmail.com>
2025-10-01 10:49:03 +0330

    fix bug

--- app/Livewire/Users/AttachRole.php
+++ app/Livewire/Users/AttachRole.php
@@ -45,7 +45,7 @@ class AttachRole extends BaseComponent
             ->leftJoin(sprintf("%s.user_roles AS  ur",$db),"user_id",'=','users.id')
             ->leftJoin(sprintf("%s.units AS u",$db),'u.id','=','ur.unit_id')
             ->select('ur.role as role2','ur.region_id','ur.unit_id','u.id AS unit_pkey','u.region_id AS unit_region_id','users.*' ,  DB::raw('COUNT(ur.id) AS roles_count'))
-            ->whereNotNull('ur.deleted_at')
+            ->whereNull('ur.deleted_at')
             ->when($this->min_roles , function (Builder $builder) {
                 $builder->having('roles_count','>=',$this->min_roles);
             })
