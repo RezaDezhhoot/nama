51747e2bd7c595a4404756273fcd5701d3417e8e
Reza Dezhhoot <rdezhhoot@gmail.com>
2025-10-17 21:19:56 +0330

    update request Inquiry

--- app/Http/Controllers/Api/V1/Inquiry/RequestController.php
+++ app/Http/Controllers/Api/V1/Inquiry/RequestController.php
@@ -11,23 +11,22 @@ class RequestController extends Controller
 {
     public function __invoke(Request $request)
     {
-        if (app()->environment('production') && $request->header('X-API-KEY') !== config('services.pusheh.api_key')) {
-            return response()->json([
-                'message' => "unauthorized",
-            ] , 401);
-        }
+//        if (app()->environment('production') && $request->header('X-API-KEY') !== config('services.pusheh.api_key')) {
+//            return response()->json([
+//                'message' => "unauthorized",
+//            ] , 401);
+//        }
         $items = [];
         $db = config('database.connections.arman.database');
-        \App\Models\Request::query()
+        $items = \App\Models\Request::query()
             ->without(['item','user','unit','members','members.image','plan'])
             ->with(['imamLetter','areaInterfaceLetter','images','otherImamLetter','otherAreaInterfaceLetter'])
             ->where('step',RequestStep::FINISH)
             ->join('request_plans AS rp','rp.id','=','requests.request_plan_id')
             ->join($db.'.users AS u','u.id','=','requests.user_id')
             ->select(['requests.*','rp.title as plan_title','u.national_id AS user_national_id'])
-            ->chunk(300 , function ($reqs) use(&$items) {
-                $items[] = $reqs;
-            });
+            ->paginate(100);
+
         return response()->json($items);
     }
 }
