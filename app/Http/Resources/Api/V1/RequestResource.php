bc081c202b4173195d2ceb9e1bb5cf3d7f6e0efc
Reza Dezhhoot <rdezhhoot@gmail.com>
2025-10-22 14:02:57 +0330

    fix bug

--- app/Http/Resources/Api/V1/RequestResource.php
+++ app/Http/Resources/Api/V1/RequestResource.php
@@ -26,7 +26,7 @@ class RequestResource extends JsonResource
             if (! in_array($this->step ,$role->step()) && in_array($this->step ,$role->next())) {
                 $status = RequestStatus::DONE;
             } elseif (in_array($role , [OperatorRole::EXECUTIVE_VICE_PRESIDENT_MOSQUES,OperatorRole::DEPUTY_FOR_PLANNING_AND_PROGRAMMING]) && ! in_array($this->step ,$role->step()) && ! in_array($this->step ,$role->next())) {
-                if ($status !== RequestStatus::IN_PROGRESS) {
+                if ($status === RequestStatus::IN_PROGRESS) {
                     $status = null;
                 }
             }
