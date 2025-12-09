1906da08edb234b331be4cab25c6c44e1abc5823
Reza Dezhhoot <rdezhhoot@gmail.com>
2025-12-09 13:12:41 +0330

    update rp

--- app/Models/RequestPlan.php
+++ app/Models/RequestPlan.php
@@ -25,7 +25,7 @@ class RequestPlan extends Model
 
     protected $guarded = [];
 
-//    protected $with = 'requirementsv';
+    protected $with = 'requirementsv';
 
     protected $casts = [
         'status' => RequestPlanStatus::class,
