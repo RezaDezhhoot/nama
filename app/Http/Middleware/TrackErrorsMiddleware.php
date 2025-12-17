3b44f29c6f64230e9c7e7570421a99b4a057e368
Reza Dezhhoot <rdezhhoot@gmail.com>
2025-12-17 05:52:54 +0330

    track errors

---
+++ app/Http/Middleware/TrackErrorsMiddleware.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpFoundation\Response;
+
+class TrackErrorsMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $response = $next($request);
+
+        if ($response->status() >= 400) {
+            Log::error('HTTP Error', [
+                'status' => $response->status(),
+                'method' => $request->method(),
+                'url' => $request->fullUrl(),
+                'headers' => $request->headers->all(),
+                'body' => $request->all(),
+                'response' => $response->getContent(),
+            ]);
+        }
+        return $response;
+    }
+}
