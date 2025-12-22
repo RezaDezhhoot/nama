44b9df54223341683bb293837c057413c28db54d
Reza Dezhhoot <rdezhhoot@gmail.com>
2025-12-22 08:55:17 +0330

    update middlewarte

--- app/Http/Controllers/Api/V1/BannerController.php
+++ app/Http/Controllers/Api/V1/BannerController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Api\V1\BannerResource;
 use App\Models\Banner;
+use App\Models\DashboardItem;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
@@ -13,7 +14,7 @@ class BannerController extends Controller
     public function __invoke(): AnonymousResourceCollection
     {
         return BannerResource::collection(
-            Banner::query()->where('item_id' , \request()->get('item_id',2))->orderBy('position')->get()
+            Banner::query()->where('item_id' , \request()->get('item_id',DashboardItem::query()->first()?->id ?? 2))->orderBy('position')->get()
         );
     }
 }
