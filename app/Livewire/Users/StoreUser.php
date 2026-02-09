9276edf8313d8a5382f55baa535df9ccc268b9e8
Reza Dezhhoot <rdezhhoot@gmail.com>
2026-02-09 07:42:25 +0330

    add profile role

--- app/Livewire/Users/StoreUser.php
+++ app/Livewire/Users/StoreUser.php
@@ -68,6 +68,7 @@ class StoreUser extends BaseComponent
         $this->data['coach_type'] = SchoolCoachType::labels();
         try {
             $res = ProfileService::new()->submission($this->user->national_id);
+            dd($res);
             $this->profileRoles = [
                 'setad_roles' => $res['setad_roles'],
                 'educational_roles' => $res['educational_roles'],
